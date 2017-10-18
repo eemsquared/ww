cad6b248346228dd6e108b39ee91886dd99e9d33
Winnah Gwen Acal <winnahacal@gmail.com>
2017-10-18 09:05:43 +0800

    haha

--- src/App.java
+++ src/App.java
@@ -19,12 +19,29 @@ public class App {
                 Timestamp.valueOf("2017-9-21 22:00:00"), "Taiwan", "Taiwan");
         Ship ship2 = new Ship("B-5", "26-33","VTB", Timestamp.valueOf("2017-9-21 7:00:00"),
                 Timestamp.valueOf("2017-9-29 7:00:00"), "Vietnam", "TBA");
-        Ship ship4 = new Ship("B-5", "26-33","Lorcon", Timestamp.valueOf("2017-9-21 7:00:00"),
-                Timestamp.valueOf("2017-9-21 19:00:00"), "Vietnam", "TBA");
+        Ship ship3 = new Ship("B-6", "25.5-44","Lorcon dumaguete", Timestamp.valueOf("2017-9-21 6:00:00"),
+                Timestamp.valueOf("2017-9-21 18:00:00"), "Manila", "Cagayan");
+        Ship ship4 = new Ship("B-6", "34.5-44","MCC Palawan", Timestamp.valueOf("2017-9-21 18:00:00"),
+                Timestamp.valueOf("2017-9-21 23:00:00"), "Manila", "Cagayan");
+        Ship ship5 = new Ship("B-8/9", "49-60","St. Pope John Paul II", Timestamp.valueOf("2017-9-21 16:30:00"),
+                Timestamp.valueOf("2017-9-21 20:00:00"), "Manila", "Cagayan");
+        Ship ship6 = new Ship("B-8/9", "54-61","Span Asia 32", Timestamp.valueOf("2017-9-21 00:01:00"),
+                Timestamp.valueOf("2017-9-21 21:00:00"), "Manila", "Cagayan");
+        Ship ship7 = new Ship("B-11", "77-78","Trans Asia 10", Timestamp.valueOf("2017-9-21 05:00:00"),
+                Timestamp.valueOf("2017-9-21 20:00:00"), "Cagayan", "Cagayan");
+        Ship ship8 = new Ship("B-14/15", "109-110","Filipinas Jagna", Timestamp.valueOf("2017-9-21 05:00:00"),
+                Timestamp.valueOf("2017-9-21 19:00:00"), "Cagayan", "Cagayan");
 
-        //pdi.insert(ship1);
-        //pdi.insert(ship2);
-        //pdi.insert(ship4);
+        pdi.insert(ship1);
+        pdi.insert(ship2);
+        pdi.insert(ship3);
+        pdi.insert(ship4);
+        pdi.insert(ship5);
+        pdi.insert(ship6);
+        pdi.insert(ship7);
+        pdi.insert(ship8);
+
+
 
         /*
         Ship shipp = pdi.selectById(2);
@@ -60,9 +77,9 @@ public class App {
         */
 
 
-        //List<Ship> ships = pdi.selectAll();
+        List<Ship> ships = pdi.selectAll();
         //List<Ship> ships = pdi.selectByName("Lorcon");
-        List<Ship> ships = pdi.selectByDate(Timestamp.valueOf("2017-9-25 7:00:00"));
+        //List<Ship> ships = pdi.selectByDate(Timestamp.valueOf("2017-9-25 7:00:00"));
         System.out.println();
         for (Ship ship: ships) {
             System.out.println(ship.getId() + ". " + ship.getBerthNumber() + " " + ship.getBollardNumber() + " " + ship.getName() + " "
